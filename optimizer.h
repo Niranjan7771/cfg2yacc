e94474a9dde5052b9461563352d224a9445bbbf5
Niranjan7771 <yalamniranjan@gmail.com>
2025-11-04 13:15:04 +0530

    Revert "docs: Polished and formatted README.md"
    This reverts commit e3d526cb27c96bb8b002a158789e2d01e7ff409e.

--- optimizer.h
+++
@@ -1,8 +0,0 @@
-#ifndef OPTIMIZER_H
-#define OPTIMIZER_H
-
-#include "ir.h"
-
-void optimize_ir(Instruction **head);
-
-#endif /* OPTIMIZER_H */
--- program_io.mc
+++
@@ -1,6 +0,0 @@
-x = 5;
-print x;
-y = x + 3;
-print y;
-input z;
-print z;
